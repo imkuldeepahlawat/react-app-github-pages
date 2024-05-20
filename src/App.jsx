975ff49f8fc4d9d3ef80c27f79ac75a289723461
imkuldeepahlawat <imkuldeepahlawat@gmail.com>
2024-05-20 22:22:41 +0530

    deploy:deployed react app with tailwind css ,on https://imkuldeepahlawat.github.io/react-app-github-pages/

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-9xl">tailwind is working with react</h1>
-    </>
+    <div className="App">
+      <h1> React app running with tailwind </h1>{" "}
+    </div>
   );
 }
 
